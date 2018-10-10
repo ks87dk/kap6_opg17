ebedd67489d4a968bfc69636988bc7adaf26f679
ks87dk <ks87dk@gmail.com>
2018-10-10 19:42:09 +0200

    Initial commit

---
+++ src/kap6_opg17.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class kap6_opg17 {
+    //Main Method
+
+    /*  (Display matrix of 0s and 1s) Write a method that displays an n-by-n matrix using
+         the following header:
+
+         public static void printMatrix(int n)
+
+         Each element is 0 or 1, which is generated randomly. Write a test program that
+         prompts the user to enter n and displays an n-by-n matrix.
+    */
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = input.nextInt();    //tildeler variablen n den indtastede værdi
+
+        printMatrix(n);
+    }
+
+
+    public static void printMatrix(int n) {
+        for (int rows = 0; rows < n; rows++) {
+            for (int cols = 0; cols < n; cols ++) {
+                System.out.print((int)(Math.random() * 2)); // printer tilfældig værdi mellem 1 0g 0
+            }
+
+        }
+    }
+}
